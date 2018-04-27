a1a7a0e3689ff13f58b866d311221abc17e57856
daking2014 <daniel.king2014@gmail.com>
2018-04-27 14:20:07 -0700

    Historical volatility estimates for all common stocks on iex

--- hvEstimation.py
+++ hvEstimation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import iexUtils
 import datetime
 import numpy as np
+from sklearn.externals import joblib
 
 DAYS_FOR_2_YR = 504
 DAYS_FOR_1_YR = 252
@@ -28,7 +29,28 @@ def historicalVolatilityOneStock(symbol):
     return computeVol2Yr(stock)
 
 def main():
+    symbolsData = iexUtils.getAllSymbols()
+    symbolsData = iexUtils.filterEnabledSymbols(symbolsData)
+    symbolsData = iexUtils.filterCommonStockSymbols(symbolsData)
+    symbols = symbolsData.symbol.tolist()
 
+    volatilities = joblib.load('historicalStockData.pkl')['volatilities']
+    idx = 0
+    for symbol in symbols:
+        print(idx)
+        if idx < 2900:
+            idx += 1
+            continue
+        if symbol in iexUtils.EXCLUDE_SYMBOLS:
+            continue
+        print(symbol)
+        vol2Yr = historicalVolatilityOneStock(symbol)
+        volatilities.loc[idx] = [symbol, vol2Yr]
+        idx += 1
+        if idx % 100 == 0:
+            joblib.dump({"lastUpdate": datetime.datetime.now(), "volatilities": volatilities}, 'historicalStockData.pkl')
+
+    joblib.dump({"lastUpdate": datetime.datetime.now(), "volatilities": volatilities}, 'historicalStockData.pkl')
 
 if __name__ == '__main__':
     main()
--- iexUtils.py
+++ iexUtils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+EXCLUDE_SYMBOLS = ["ALZH", "CDAY", "DFBH", "ELOX", "LASR", "LTN^"]
+
 def getAllSymbols():
     return pd.read_json("https://api.iextrading.com/1.0/ref-data/symbols")
 
@@ -11,3 +13,8 @@ def filterCommonStockSymbols(symbols):
 
 def getHistoricalData(symbol, timeSpan):
     return pd.read_json("https://api.iextrading.com/1.0/stock/"+symbol+"/chart/"+timeSpan)
+
+def getBatchHistoricalData(symbols, timeSpan):
+    assert len(symbols) <= 100
+    print("Getting batch hv")
+    return pd.read_json("https://api.iextrading.com/1.0/stock/market/batch?symbols="+",".join(symbols)+"&types=chart&range="+timeSpan)
