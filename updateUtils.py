787069cb283580f3a0daeabd9f5da03a71c08eed
Daniel King <daniel.king2014@gmail.com>
2018-05-07 02:09:07 -0700

    update utilities

--- updateUtils.py
+++ updateUtils.py
@@ -6,6 +6,43 @@ import datetime
 from earnings import Earning
 import pandas as pd
 
+def isMonthOld(date):
+    delta = datetime.timedelta(days=31)
+    newDate = date + delta
+    return datetime.datetime.now() > newDate
+
+def updateSymbol(sym):
+    stockData = joblib.load('historicalStockData.pkl')['data']
+    try:
+        earningsData = iexUtils.getEarnings(sym)['earnings']
+    except:
+        stockData.loc[sym, 'earningsUpdated'] = datetime.datetime.now()
+        return
+
+    earnings = []
+    for earningData in earningsData:
+        if earningData['actualEPS'] == None or earningData['consensusEPS'] == None or earningData['estimatedEPS'] == None or earningData['announceTime'] == None or earningData['fiscalPeriod'] == None:
+            stockData.loc[sym, 'earningsUpdated'] = datetime.datetime.now()
+            continue
+        reportDate = datetime.datetime.strptime(earningData['EPSReportDate'], "%Y-%m-%d")
+        if reportDate >= datetime.datetime.strptime("2018-05-04", "%Y-%m-%d"):
+            stockData.loc[sym, 'earningsUpdated'] = datetime.datetime.now()
+            continue
+        earning = Earning(earningData['actualEPS'], earningData['consensusEPS'], earningData['estimatedEPS'], earningData['announceTime'], earningData['EPSSurpriseDollar'], reportDate, earningData['fiscalPeriod'])
+        earning.addOHLC(sym)
+        earnings.append(earning)
+    stockData.loc[sym, 'earnings'] = earnings
+    stockData.loc[sym, 'earningsUpdated'] = datetime.datetime.now()
+
+    vol2Yr, drift2Yr =historicalVolatilityAndDriftOneStock(sym)
+    stockData.loc[sym, 'vols2Yr'] = vol2Yr
+    stockData.loc[sym, 'vols2YrUpdated'] = datetime.datetime.now()
+    stockData.loc[sym, 'drift2Yr'] = drift2Yr
+    stockData.loc[sym, 'drift2YrUpdated'] = datetime.datetime.now()
+
+    joblib.dump({"data": stockData}, 'historicalStockData.pkl')
+
+
 def addNewSymbols():
     symbolsData = iexUtils.getAllSymbols()
     symbolsData = iexUtils.filterEnabledSymbols(symbolsData)
